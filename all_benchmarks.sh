6c31307072ce6cffd7e557124d1e8d749481036c
Benjamin Berendsohn <benjamin.berendsohn@fu-berlin.de>
2023-07-24 17:15:50 +0200

    Add new benchmarks to all_benchmarks and show_results scripts

--- all_benchmarks.sh
+++ all_benchmarks.sh
@@ -10,3 +10,7 @@ bash benchmark_queries_uniform_large.sh
 bash benchmark_degenerate.sh
 bash benchmark_degenerate_noisy.sh
 bash benchmark_mst.sh
+bash benchmark_fd_con.sh
+bash benchmark_lca.sh
+bash benchmark_num_rotations.sh
+bash benchmark_queries_path_prob.sh
--- show_results.sh
+++ show_results.sh
@@ -2,8 +2,12 @@
 
 python3 show_benchmarks/visualize.py --input-file results/queries_uniform.jsonl --profile queries-uniform
 python3 show_benchmarks/visualize.py --input-file results/queries_uniform_large.jsonl --profile queries-uniform
+python3 show_benchmarks/visualize.py --input-file results/queries_path_prob.jsonl --profile queries-path-prob
 
 python3 show_benchmarks/visualize.py --input-file results/degenerate.jsonl --profile degenerate
 python3 show_benchmarks/visualize.py --input-file results/degenerate_noisy.jsonl --profile degenerate-noisy
 
 python3 show_benchmarks/visualize.py --input-file results/mst.jsonl --profile mst-vertices
+
+python3 show_benchmarks/visualize.py --input-file results/fd_con.jsonl --profile fd-con
+
